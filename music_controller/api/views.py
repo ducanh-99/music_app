23ccdb4cadf762325d62a2b771d725af7e48ed70
ducanh <ducanhhust99@gmail.com>
2021-01-02 07:39:41 +0700

    set up django server

---
+++ music_controller/api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
+# Create your views here.
+
+
+# def main(request):
+#     return HttpResponse("hello")
+
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
